ac8d9a7c0575a932772b62b17088ba8226411ef7
szbmrk <szmark16@gmail.com>
2023-07-27 13:56:13 +0200

    signup v0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import Example from "./components/Example";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/user/Login';
+import Signup from './components/user/Signup';
 
-function App() {
+const App = () => {
     return (
-        <div>
-            <Example />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
---
+++ frontend/src/components/user/Signup.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import '../../styles/signup.css';
+import '../../api/axios';
+
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        acceptedTerms: false,
+    });
+
+    const handlePaste = (e) => {
+        e.preventDefault();
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            ['confirmPassword']: '',
+        }));
+    };
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        const val = type === 'checkbox' ? checked : value;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: val,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // post a axios request to the backend
+        console.log(formData);
+    };
+
+    return (
+        <form className="signup-form" onSubmit={handleSubmit}>
+            <h2>Sign Up</h2>
+            <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    required
+                    onPaste={handlePaste}
+                />
+            </div>
+            <div className="form-group">
+                <label>
+                    <input
+                        type="checkbox"
+                        name="acceptedTerms"
+                        checked={formData.acceptedTerms}
+                        onChange={handleChange}
+                        required
+                    />{' '}
+                    I accept the terms and conditions
+                </label>
+            </div>
+            <button type="submit">Sign Up</button>
+        </form>
+    );
+};
+
+export default Signup;
