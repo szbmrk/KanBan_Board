8dbc6157f1622a8157f0dcce2e4be49f717dc2ee
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-07-27 14:40:18 +0200

    signup page: added background, shadows

--- frontend/src/components/user/Signup.js
+++ frontend/src/components/user/Signup.js
@@ -36,68 +36,70 @@ const Signup = () => {
     };
 
     return (
-        <form className="signup-form" onSubmit={handleSubmit}>
-            <Link to="/login">Already have an account?</Link>
-            <h2>Sign Up</h2>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    required
-                    onPaste={handlePaste}
-                />
-            </div>
-            <div className="form-group">
-                <label>
+        <div className="background">
+            <form className="signup-form" onSubmit={handleSubmit}>
+                <Link to="/login">Already have an account?</Link>
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
                     <input
-                        type="checkbox"
-                        name="acceptedTerms"
-                        checked={formData.acceptedTerms}
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
                         onChange={handleChange}
                         required
-                    />{' '}
-                    I accept the terms and conditions
-                </label>
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                        onPaste={handlePaste}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="acceptedTerms"
+                            checked={formData.acceptedTerms}
+                            onChange={handleChange}
+                            required
+                        />{' '}
+                        I accept the terms and conditions
+                    </label>
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
     );
 };
 
