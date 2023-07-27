1215914d646b007d21ae7a08bec28887f5cc611f
szbmrk <szmark16@gmail.com>
2023-07-27 11:45:01 +0200

    package update

--- frontend/src/components/Example.js
+++ frontend/src/components/Example.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 export default function Example() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/test')
+            .then(res => {
+                setData(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
     return (
-        <div>Example</div>
+        <div>{data}</div>
     )
 }
