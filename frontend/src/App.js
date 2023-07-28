31e3793d4148a1013818e3e9d770c5d427c5aad9
szbmrk <szmark16@gmail.com>
2023-07-28 11:46:19 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/user/Login';
 import Signup from './components/user/Signup';
-import axios from './api/axios'
 import Dashboard from './components/Dashboard';
 import DragDrop from './components/DragDrop';
 import AuthProvider from "./auth/AuthProvider";
@@ -16,7 +14,7 @@ const App = () => {
                     <Route path="/" element={<ProtectedRoute> <Navigate to="/dashboard" /> </ProtectedRoute>} />
                     <Route exact path="/login" element={<Login />} />
                     <Route exact path="/signup" element={<Signup />} />
-                    <Route exact path="/test" element={<DragDrop />} />
+                    <Route exact path="/board" element={<DragDrop />} />
                     <Route exact path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
                 </Routes>
             </AuthProvider>
