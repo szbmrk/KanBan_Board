775787cdc06467600da92c56d01a7b058dcc13f7
szbmrk <szmark16@gmail.com>
2023-07-28 13:40:01 +0200

    user_id added to session

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,10 +5,11 @@ export default function Dashboard() {
 
     useEffect(() => {
         const token = sessionStorage.getItem('token');
+        const user_id = sessionStorage.getItem('user_id');
         const username = sessionStorage.getItem('username');
         const email = sessionStorage.getItem('email');
-        if (token && username && email) {
-            setUser({ username: username, email: email, token: token });
+        if (token && user_id && username && email) {
+            setUser({ user_id: user_id, username: username, email: email, token: token });
         }
     }, []);
 
@@ -16,6 +17,7 @@ export default function Dashboard() {
         <div>
             {user && <div>
                 <h1>Welcome {user.username}</h1>
+                <h2>Your user id is {user.user_id}</h2>
                 <h2>Your email is {user.email}</h2>
                 <h3>Your token is {user.token}</h3>
             </div>}
