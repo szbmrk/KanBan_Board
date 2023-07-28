d7acea11bb507c211dd92313ee9cb7a28e169d93
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-07-28 09:17:23 +0200

    Teams seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,10 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Database\Factories\UserFactory;
-
 use App\Models\User;
+use Illuminate\Database\Seeder;
+
+use Database\Factories\UserFactory;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\TeamsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,10 @@ class DatabaseSeeder extends Seeder
 
         // Create 5 users
         UserFactory::new()->count(5)->create();
+
+        $this->call([
+            RolesTableSeeder::class,
+            TeamsTableSeeder::class
+        ]);
     }
 }
---
+++ backend/database/seeders/TeamsTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TeamsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Seed the 'teams' table with sample data
+        $teams = [
+            [
+                'name' => 'Team 1',
+                'created_by' => 1, 
+                'parent_team_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Team 2',
+                'created_by' => 2, 
+                'parent_team_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Team 3 (Subteam of Team 1)',
+                'created_by' => 3, 
+                'parent_team_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+
+
+        ];
+
+
+        DB::table('teams')->insert($teams);
+    }
+}
