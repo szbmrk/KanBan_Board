4ac7dd19d07fcaf7578592e7c9ac3c7a7d2d139b
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-07-31 21:59:41 +0200

    sidebar styling

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,19 +3,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faTable, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { AuthContext } from '../auth/AuthContext';
 
 const homeIcon = <FontAwesomeIcon icon={faHome} />;
-const userIcon = <FontAwesomeIcon icon={faUser} />;
+const userIcon = <FontAwesomeIcon icon={faTable} />;
 const signOutIcon = <FontAwesomeIcon icon={faSignOutAlt} />;
 const Sidebar = () => {
     const { isLoggedIn, onLogout } = React.useContext(AuthContext);
     return (
         <div className="sidebar">
-            <div className="sidebar-header col-12 col-s-12">
-                <h3>KanBan Board</h3>
-            </div>
             <div className="sidebar-menu col-2 col-s-2">
                 <ul>
                     <li>
