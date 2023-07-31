ca54700b15e0867fd58ca4d87145c26e0e2173fe
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-07-31 21:05:18 +0200

    added sidebar to the project

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,45 @@
+// Sidebar component that using among the application
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../auth/AuthContext';
+
+const homeIcon = <FontAwesomeIcon icon={faHome} />;
+const userIcon = <FontAwesomeIcon icon={faUser} />;
+const signOutIcon = <FontAwesomeIcon icon={faSignOutAlt} />;
+const Sidebar = () => {
+    const { isLoggedIn, onLogout } = React.useContext(AuthContext);
+    return (
+        <div className="sidebar">
+            <div className="sidebar-header">
+                <h3>KanBan Board</h3>
+            </div>
+            <div className="sidebar-menu">
+                <ul>
+                    <li>
+                        <Link to="/dashboard">
+                            {homeIcon}
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/board">
+                            {userIcon}
+                            <span>Board</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login" onClick={onLogout}>
+                            {signOutIcon}
+                            <span>Logout</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
