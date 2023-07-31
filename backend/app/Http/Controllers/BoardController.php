caa6f66278ce82157f8268409ea7751c0f5eaa7d
Vamos Akos <vamos.akos1337@gmail.com>
2023-07-31 15:41:53 +0200

    added taskStore function

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Board;
 use App\Models\Column;
 use App\Models\Task;
+use Illuminate\Validation\Rule;
 
 class BoardController extends Controller
 {
@@ -80,5 +81,54 @@ class BoardController extends Controller
     
             return response()->json(['column' => $column]);
         }
+    }
+
+    public function taskStore(Request $request, $board_id)
+    {
+        $user = auth()->user();
+        $board = Board::find($board_id);
+    
+        if (!$board) {
+            return response()->json(['error' => 'Board not found'], 404);
+        }
+    
+        if (!$user->isMemberOfBoard($board_id)) {
+            return response()->json(['error' => 'You are not a member of this board'], 403);
+        }
+    
+        $this->validate($request, [
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+            'column_id' => [
+                'required',
+                'integer',
+                Rule::exists('columns', 'column_id')->where(function ($query) use ($board_id) {
+                    $query->where('board_id', $board_id);
+                }),
+            ],
+            'priority_id' => 'nullable|integer|exists:priorities,priority_id',
+        ]);
+    
+        // Find the last task position in the column
+        $lastTask = Task::where('column_id', $request->input('column_id'))
+            ->orderBy('position', 'desc')
+            ->first();
+    
+        $position = $lastTask ? $lastTask->position + 1 : 0;
+    
+        $task = new Task([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'due_date' => $request->input('due_date'),
+            'column_id' => $request->input('column_id'),
+            'project_id' => $board->project_id,
+            'priority_id' => $request->input('priority_id'),
+            'position' => $position, // Set the position to the next available position
+        ]);
+    
+        $task->save();
+    
+        return response()->json(['message' => 'Task created successfully', 'task' => $task]);
     }
 }
