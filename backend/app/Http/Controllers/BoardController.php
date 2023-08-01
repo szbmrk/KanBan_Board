28157d9d75aec0428636d013b22a1054d4f2e691
vamosmarton <vammarton@gmail.com>
2023-08-01 09:37:49 +0200

    updated columnPositionUpdate and fixed columnStore

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -32,33 +32,33 @@ class BoardController extends Controller
     {
         $user = auth()->user();
         $board = Board::find($board_id);
-
+    
         if (!$board) {
             return response()->json(['error' => 'Board not found'], 404);
         }
-
+    
         if (!$user->isMemberOfBoard($board_id)) {
             return response()->json(['error' => 'You are not a member of this board'], 403);
         }
-
+    
         $this->validate($request, [
-            'position' => 'required|integer',
+            'name' => 'required|string|max:50',
         ]);
-
-        //check if there is already a column with the same position)
-        if($column = $board->columns()->where('position', $request->input('position'))->first() != null){
-            return response()->json(['error' => 'There is already a column with this position'], 403);
-        }
-
+    
+        $maxPosition = $board->columns()->max('position');
+    
         $column = new Column([
-            'position' => $request->input('position'),
+            'name' => $request->input('name'),
+            'position' => $maxPosition !== null ? $maxPosition + 1 : 0,
             'board_id' => $board->board_id,
+            'is_finished' => $request->input('is_finished', false),
+            'task_limit' => $request->input('task_limit', null),
         ]);
-
+    
         $board->columns()->save($column);
-
+    
         return response()->json(['message' => 'Column created successfully', 'column' => $column]);
-    }
+    }    
     
     public function columnUpdate(Request $request, $column_id)
     {
@@ -86,17 +86,21 @@ class BoardController extends Controller
     {
         $user = auth()->user();
         $board = Board::find($board_id);
-
+    
         if (!$board) {
             return response()->json(['error' => 'Board not found'], 404);
         }
-
+    
         if (!$user->isMemberOfBoard($board_id)) {
             return response()->json(['error' => 'You are not a member of this board'], 403);
         }
-
+    
         $columns = $request->columns;
-
+    
+        if (count($columns) !== count(array_unique($columns))) {
+            return response()->json(['error' => 'Duplicate positions are not allowed'], 400);
+        }
+    
         foreach ($columns as $position => $column_id) {
             $column = Column::find($column_id);
             if ($column && $column->board_id == $board_id) {
@@ -106,7 +110,7 @@ class BoardController extends Controller
                 return response()->json(['error' => 'Column not found or not belong to this board'], 404);
             }
         }
-
+    
         return response()->json(['message' => 'Columns position updated successfully.']);
-    }
+    }    
 }
