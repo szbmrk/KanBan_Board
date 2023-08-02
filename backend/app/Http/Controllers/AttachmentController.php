fb0d5a5308fd27d2bbd57ea679030d052a24d877
Vamos Akos <vamos.akos1337@gmail.com>
2023-08-02 15:27:07 +0200

    added attachments crud functions

---
+++ backend/app/Http/Controllers/AttachmentController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attachment;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AttachmentController extends Controller
+{
+    public function index(Request $request, $task_id)
+    {
+        $user = auth()->user();
+        $task = Task::find($task_id);
+
+        if (!$task) {
+            return response()->json(['error' => 'Task not found'], 404);
+        }
+
+        $board = $task->column->board;
+
+        if (!$user->isMemberOfBoard($board->board_id)) {
+            return response()->json(['error' => 'You are not a member of this board'], 403);
+        }
+
+        $attachments = Attachment::where('task_id', $task_id)->get();
+
+        if ($attachments->isEmpty()) {
+            return response()->json(['message' => 'No attachments found for this task'], 200);
+        }
+
+        return response()->json(['attachments' => $attachments]);
+    }
+
+    public function store(Request $request, $task_id)
+    {
+        $user = auth()->user();
+        $task = Task::find($task_id);
+
+        if (!$task) {
+            return response()->json(['error' => 'Task not found'], 404);
+        }
+
+        $board = $task->column->board;
+
+        if (!$user->isMemberOfBoard($board->board_id)) {
+            return response()->json(['error' => 'You are not a member of this board'], 403);
+        }
+
+        $request->validate([
+            'link' => 'required|string',
+        ]);
+
+        $attachment = new Attachment([
+            'task_id' => $task_id,
+            'link' => $request->input('link'),
+            'description' => $request->input('description'),
+        ]);
+
+        $attachment->save();
+
+        return response()->json(['message' => 'Attachment created successfully', 'attachment' => $attachment]);
+    }
+
+    public function update(Request $request, $attachment_id)
+    {
+        $user = auth()->user();
+        $attachment = Attachment::find($attachment_id);
+
+        if (!$attachment) {
+            return response()->json(['error' => 'Attachment not found'], 404);
+        }
+
+        $task = $attachment->task;
+        $board = $task->column->board;
+
+        if (!$user->isMemberOfBoard($board->board_id)) {
+            return response()->json(['error' => 'You are not a member of this board'], 403);
+        }
+
+        $request->validate([
+            'link' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+        
+        $attachment->update([
+            'link' => $request->input('link'),
+            'description' => $request->input('description'),
+        ]);
+
+        return response()->json(['message' => 'Attachment updated successfully', 'attachment' => $attachment]);
+    }
+
+    public function destroy($attachment_id)
+    {
+        $user = Auth::user();
+        $attachment = Attachment::find($attachment_id);
+
+        if (!$attachment) {
+            return response()->json(['error' => 'Attachment not found'], 404);
+        }
+
+        $task = $attachment->task;
+        $board = $task->column->board;
+
+        if (!$user->isMemberOfBoard($board->board_id)) {
+            return response()->json(['error' => 'You are not a member of this board'], 403);
+        }
+
+        $attachment->delete();
+
+        return response()->json(['message' => 'Attachment deleted successfully']);
+    }
+}
