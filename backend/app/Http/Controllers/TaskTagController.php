ae5dc28337052f05084ecec740c60fd1d005e495
rcfr84 <valcsi20010923@gmail.com>
2023-08-02 13:24:23 +0200

    Remove unused function

--- backend/app/Http/Controllers/TaskTagController.php
+++ backend/app/Http/Controllers/TaskTagController.php
@@ -7,13 +7,5 @@ use App\Models\TaskTag;
 
 class TaskTagController extends Controller
 {
-    public function index($task_id)
-    {
-        if (!auth()->check()) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-        $tags = TaskTag::where('task_id', $task_id)->get();
-
-        return response()->json($tags);
-    }
+    
 }
