8b887606afc0ef5afffa45f0493f4bbb4a4584cf
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-02 09:34:16 +0200

    adding menu button to the navbar

--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faUser, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const notificationIcon = <FontAwesomeIcon icon={faBell} />;
 const profileIcon = <FontAwesomeIcon icon={faUser} />;
+const menuIcon = <FontAwesomeIcon icon={faBars} />;
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-menu col-12 col-s-12">
+                <button id="menu-btn">{menuIcon}</button>
                 <ul>
                     <li>
                         <Link to="/notifications">
