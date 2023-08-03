ba3053820635daf7132f493f8fc8e64448a385ab
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-08-03 12:53:06 +0200

    Invalid request

--- backend/app/Http/Controllers/AGIController.php
+++ backend/app/Http/Controllers/AGIController.php
@@ -16,25 +16,34 @@ class AGIController extends Controller
 
         $task = "React backend development - login page";
         $column = "In progress";
-    
+
         // Extract the prompt from the JSON payload
         $prompt = "Generate kanban tickets for {$task}. Write estimations to the tickets as well and add a tagg to each ticket. The tickets should be in the column '{$column}'.Write a descreption to each of them as well";
-    
+
         $maxTokens = 100; // Adjust this value based on your requirement
-    
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $apiKey",
-            'Content-Type' => 'application/json',
-        ])->post('https://api.openai.com/v1/engines/text-davinci-003/completions', [
-            'prompt' => $prompt,
-            'max_tokens' => $maxTokens,
-        ]);
-    
-        // Handle the response here (e.g., extract the generated subtask from the response).
-        $subtask = $response['choices'][0]['text'];
-    
-        // Return the subtask as a JSON response to the frontend
-        return response()->json(['subtask' => $subtask]);
+
+        try {
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer $apiKey",
+                'Content-Type' => 'application/json',
+            ])->post('https://api.openai.com/v1/engines/text-davinci-003/completions', [
+                'prompt' => $prompt,
+                'max_tokens' => $maxTokens,
+            ]);
+
+            $responseData = $response->json();
+
+            if (isset($responseData['choices']) && !empty($responseData['choices'])) {
+                // Handle the response here (e.g., extract the generated subtask from the response).
+                $subtask = $responseData['choices'][0]['text'];
+
+                // Return the subtask as a JSON response to the frontend
+                return response()->json(['subtask' => $subtask]);
+            } else {
+                return response()->json(['error' => 'Invalid response from OpenAI API']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
     }
-    
 }
