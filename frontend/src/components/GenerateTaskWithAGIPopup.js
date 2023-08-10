ae5a3381d1395e1be6ed9804c4a6a6b3bbba67c9
s-mate <megamate95@gmail.com>
2023-08-10 15:47:24 +0200

    Counter option bug fix

--- frontend/src/components/GenerateTaskWithAGIPopup.js
+++ frontend/src/components/GenerateTaskWithAGIPopup.js
@@ -19,7 +19,18 @@ const GenerateTaskWithAGIPopup = ({ tasks, onCancel }) => {
     { value: "llama", label: "Llama" },
   ];
   let [chosenAI, setChosenAI] = useState(aiOptions[0]);
-  const counterOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const counterOptions = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" },
+    { value: "6", label: "6" },
+    { value: "7", label: "7" },
+    { value: "8", label: "8" },
+    { value: "9", label: "9" },
+    { value: "10", label: "10" },
+  ];
   let [taskCounter, setTaskCounter] = useState(counterOptions[0]);
 
   const closeIcon = <FontAwesomeIcon icon={faXmark} />;
