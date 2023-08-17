df0a99f17960df1abd517d5f9641e0ee850e9377
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-17 16:06:45 +0200

    changed permission table icon

--- frontend/src/components/Navigation/SideBar.js
+++ frontend/src/components/Navigation/SideBar.js
@@ -7,44 +7,44 @@ import { faHome, faTable, faSignOutAlt, faListCheck, faPeopleGroup } from '@fort
 import { AuthContext } from '../../auth/AuthContext';
 
 const homeIcon = <FontAwesomeIcon icon={faHome} />;
-const userIcon = <FontAwesomeIcon icon={faTable} />;
+const tableIcon = <FontAwesomeIcon icon={faTable} />;
 const signOutIcon = <FontAwesomeIcon icon={faSignOutAlt} />;
 const AssignedTasksIcon = <FontAwesomeIcon icon={faListCheck} />;
-const PeopleGroup = <FontAwesomeIcon icon={faPeopleGroup} />
+const PeopleGroup = <FontAwesomeIcon icon={faPeopleGroup} />;
 const Sidebar = () => {
     const { isLoggedIn, onLogout } = React.useContext(AuthContext);
     return (
-        <div className="sidebar">
-            <div className="sidebar-menu col-2 col-s-2">
+        <div className='sidebar'>
+            <div className='sidebar-menu col-2 col-s-2'>
                 <ul>
                     <li>
-                        <Link to="/dashboard">
+                        <Link to='/dashboard'>
                             {homeIcon}
                             <span>Dashboard</span>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to="/assigned_tasks">
+                        <Link to='/assigned_tasks'>
                             {AssignedTasksIcon}
                             <span>Assigned Tasks</span>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to="/teams">
+                        <Link to='/teams'>
                             {PeopleGroup}
                             <span>Teams</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/permissiontable">
-                            {signOutIcon}
+                        <Link to='/permissiontable'>
+                            {tableIcon}
                             <span>Permission table</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/login" onClick={onLogout}>
+                        <Link to='/login' onClick={onLogout}>
                             {signOutIcon}
                             <span>Logout</span>
                         </Link>
