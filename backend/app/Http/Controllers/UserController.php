95408b73845bfdeaa9b10c9a36b774e0385cb0c6
szbmrk <szmark16@gmail.com>
2023-08-17 08:39:52 +0200

    Update UserController.php

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 use App\Models\Role;
 use App\Models\Permission;
 use App\Models\TeamMember;
