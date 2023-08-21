409fa81550f79a25004f68c64a1182635392a058
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-21 15:29:14 +0200

    styling profile's submenu

--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -4,12 +4,13 @@ import { AuthContext } from '../../auth/AuthContext';
 import '../../styles/navbar.css';
 import '../../styles/popup.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faUser, faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faUser, faBars, faSignOutAlt, faUserPen } from '@fortawesome/free-solid-svg-icons';
 
 const notificationIcon = <FontAwesomeIcon icon={faBell} />;
 const profileIcon = <FontAwesomeIcon icon={faUser} />;
 const menuIcon = <FontAwesomeIcon icon={faBars} />;
 const signOutIcon = <FontAwesomeIcon icon={faSignOutAlt} />;
+const editProfileIcon = <FontAwesomeIcon icon={faUserPen} />;
 
 const Navbar = () => {
     const { isLoggedIn, onLogout } = React.useContext(AuthContext);
@@ -26,17 +27,15 @@ const Navbar = () => {
     };
 
     return (
-        <div>
-            <div className="navbar">
-                <div className="navbar-menu col-12 col-s-12">
-                    <button
-                        id="menu-btn"
-                        onClick={hideSidebar}>
+        <>
+            <div className='navbar'>
+                <div className='navbar-menu col-12 col-s-12'>
+                    <button id='menu-btn' onClick={hideSidebar}>
                         {menuIcon}
                     </button>
                     <ul>
                         <li>
-                            <Link to="/notifications">
+                            <Link to='/notifications'>
                                 <span>{notificationIcon}</span>
                             </Link>
                         </li>
@@ -47,28 +46,25 @@ const Navbar = () => {
                 </div>
             </div>
             {isOpen && (
-                <table className='menu'>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <Link to="/profile">
-                                    <span>Profile</span>
-                                </Link>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <Link to='/login' onClick={onLogout}>
-                                    {signOutIcon}
-                                    <span> Logout</span>
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            )
-            }
-        </div>
+                <div className='profile-submenu'>
+                    <p className='profile-menu-title'> Profile </p>
+                    <ul className='profile-menu'>
+                        <li>
+                            <Link to='/profile' onClick={toggleDropdown}>
+                                <span>{editProfileIcon}</span>
+                                <span>Edit Profile</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/login' onClick={[onLogout, toggleDropdown]} className='logout'>
+                                <span>{signOutIcon}</span>
+                                <span>Sign Out</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </>
     );
 };
 
