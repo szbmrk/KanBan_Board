9f98a63c2220b34c9ae582d3611c1960b9a2cacf
szbmrk <szmark16@gmail.com>
2023-08-22 12:07:01 +0200

    Update Permission.php

--- backend/app/Models/Permission.php
+++ backend/app/Models/Permission.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
-    
+
+    protected $fillable = [
+        'name'
+    ];
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'permission_role', 'permission_id', 'role_id');
