e8b46c184acd0c02802ccb9d3479222ba514535e
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-22 10:25:05 +0200

    updated notifications style

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -11,7 +11,7 @@ export default function Notification() {
     const token = sessionStorage.getItem('token');
     const userId = sessionStorage.getItem('user_id');
 
-    const checkIcon = <FontAwesomeIcon icon={faCheck} />
+    const checkIcon = <FontAwesomeIcon icon={faCheck} />;
 
     useEffect(() => {
         document.title = 'Notification';
@@ -32,39 +32,41 @@ export default function Notification() {
         notifications.forEach(async (notification) => {
             if (notification.is_read === 0) {
                 try {
-                    const res = await axios.put(`/notifications/${notification.notification_id}`,
+                    const res = await axios.put(
+                        `/notifications/${notification.notification_id}`,
                         {
-                            is_read: 1
+                            is_read: 1,
                         },
                         {
                             headers: {
                                 Authorization: `Bearer ${token}`,
                             },
-                        });
-                }
-                catch (err) {
+                        }
+                    );
+                } catch (err) {
                     console.log(err);
                 }
             }
         });
         const newNotifications = notifications.map((item) => {
             return { ...item, is_read: 1 };
-        }
-        );
+        });
         setNotifications(newNotifications);
     };
 
     const markAsSeen = async (notification) => {
         try {
-            const res = await axios.put(`/notifications/${notification.notification_id}`,
+            const res = await axios.put(
+                `/notifications/${notification.notification_id}`,
                 {
-                    is_read: 1
+                    is_read: 1,
                 },
                 {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
-                });
+                }
+            );
             const newNotifications = notifications.map((item) => {
                 if (item.notification_id === notification.notification_id) {
                     return { ...item, is_read: 1 };
@@ -72,8 +74,7 @@ export default function Notification() {
                 return item;
             });
             setNotifications(newNotifications);
-        }
-        catch (err) {
+        } catch (err) {
             console.log(err);
         }
     };
@@ -86,61 +87,110 @@ export default function Notification() {
     const countOfseen = notifications.filter((notification) => notification.is_read === 1).length;
 
     const dateFormatOptions = {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "numeric",
-        minute: "2-digit",
-        hour12: true
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true,
     };
 
     return (
         <div className='content'>
             {notifications.length === 0 ? (
                 <Loader />
-            ) : <div className='container'>
-                <h1 className='header'>Notifications</h1>
-                <button className='switch' onClick={toggleSwitch}>{isRead ? 'Switch to unseen' : 'Switch to seen'}</button>
-                {isRead === false ? <>
-                    <div className="notification-container">
-                        {countOfUnseen > 0 ?
-                            <>
-                                <button className="mark-all-button" onClick={markAllAsSeen}>{checkIcon} Mark All as Seen</button>
-                                {notifications.map((notification) => (
-                                    notification.is_read === 0 && <div
-                                        key={notification.notification_id}
-                                        className={`notification`}
+            ) : (
+                <>
+                    <h1>Notifications</h1>
+                    <div className='switch-container'>
+                        <button className='switch' onClick={toggleSwitch}>
+                            {isRead ? 'Switch to unseen' : 'Switch to seen'}
+                        </button>
+                    </div>
+                    {isRead === false ? (
+                        <>
+                            <div className='notification-container'>
+                                <div className='container-header'>
+                                    <p>Notification History</p>
+                                    <button
+                                        className='mark-all-button'
+                                        onClick={markAllAsSeen}
+                                        style={{
+                                            display: countOfUnseen > 0 ? 'block' : 'none',
+                                        }}
                                     >
-                                        <div className='notification-header'>
-                                            <span className="created-at">{new Date(notification.created_at).toLocaleString("en-US", dateFormatOptions)}</span>
-                                            <span className="notification-type">{notification.type}</span>
-                                        </div>
-                                        <p className="notification-content">{notification.content}</p>
-                                        <button className="mark-seen-button" onClick={() => markAsSeen(notification)}>{checkIcon} Mark as Seen</button>
-                                    </div>)
+                                        {checkIcon} Mark All as Seen
+                                    </button>
+                                </div>
+                                {countOfUnseen > 0 ? (
+                                    <>
+                                        {notifications.map(
+                                            (notification) =>
+                                                notification.is_read === 0 && (
+                                                    <div key={notification.notification_id} className={`notification`}>
+                                                        <div className='notification-header'>
+                                                            <span className='created-at'>
+                                                                {new Date(notification.created_at).toLocaleString(
+                                                                    'en-US',
+                                                                    dateFormatOptions
+                                                                )}
+                                                            </span>
+                                                            <span className='notification-type'>
+                                                                {notification.type}
+                                                            </span>
+                                                        </div>
+                                                        <p className='notification-content'>{notification.content}</p>
+                                                        <button
+                                                            className='mark-seen-button'
+                                                            onClick={() => markAsSeen(notification)}
+                                                        >
+                                                            {checkIcon} Mark as Seen
+                                                        </button>
+                                                    </div>
+                                                )
+                                        )}
+                                    </>
+                                ) : (
+                                    <p className='not-found-message'>No unseen notifications found!</p>
                                 )}
-                            </> : <h3 className='header'>No unseen notifications found!</h3>
-                        }
-                    </div>
-                </> : <>
-                    <div className="notification-container">
-                        {countOfseen > 0 ? <>
-                            {notifications.map((notification) => (
-                                notification.is_read === 1 && <div
-                                    key={notification.notification_id}
-                                    className={`notification`}
-                                >
-                                    <div className='notification-header'>
-                                        <span className="created-at">{new Date(notification.created_at).toLocaleString("en-US", dateFormatOptions)}</span>
-                                        <span className="notification-type">{notification.type}</span>
-                                    </div>
-                                    <p className="notification-content">{notification.content}</p>
-                                </div>)
-                            )}
-                        </> : <h3 className='header'>No seen notifications found!</h3>}
-                    </div>
-                </>}
-            </div>}
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className='notification-container'>
+                                <div className='container-header'>
+                                    <p>Notification History</p>
+                                </div>
+                                {countOfseen > 0 ? (
+                                    <>
+                                        {notifications.map(
+                                            (notification) =>
+                                                notification.is_read === 1 && (
+                                                    <div key={notification.notification_id} className={`notification`}>
+                                                        <div className='notification-header'>
+                                                            <span className='created-at'>
+                                                                {new Date(notification.created_at).toLocaleString(
+                                                                    'en-US',
+                                                                    dateFormatOptions
+                                                                )}
+                                                            </span>
+                                                            <span className='notification-type'>
+                                                                {notification.type}
+                                                            </span>
+                                                        </div>
+                                                        <p className='notification-content'>{notification.content}</p>
+                                                    </div>
+                                                )
+                                        )}
+                                    </>
+                                ) : (
+                                    <p className='not-found-message'>No seen notifications found!</p>
+                                )}
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
         </div>
     );
 }
