2bd4f547155511d2ce030e2e1a5f0f88facdc243
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-23 14:49:09 +0200

    added team member cards to teams

--- frontend/src/components/Teams/TeamCard.js
+++ frontend/src/components/Teams/TeamCard.js
@@ -7,7 +7,17 @@ import Loader from '../Loader';
 import RolesManager from './RolesManager';
 import axios from '../../api/axios';
 
-const TeamCard = ({ data, deleteUserFromTeam, ChangeTeamName, AddUsers, DeleteTeam, ownPermissions, teamPermissions, handleDeleteRole, AddRoleToUser }) => {
+const TeamCard = ({
+    data,
+    deleteUserFromTeam,
+    ChangeTeamName,
+    AddUsers,
+    DeleteTeam,
+    ownPermissions,
+    teamPermissions,
+    handleDeleteRole,
+    AddRoleToUser,
+}) => {
     const [manageIsClicked, setManage] = useState(false);
     const [deleteIsClicked, setDelete] = useState(false);
     const [addIsClicked, setAdd] = useState(false);
@@ -60,7 +70,7 @@ const TeamCard = ({ data, deleteUserFromTeam, ChangeTeamName, AddUsers, DeleteTe
                 }
             }
         }
-    }
+    };
 
     const checkPermissonToManageTeamMembers = (team_id) => {
         //TODO Refactor
@@ -77,7 +87,7 @@ const TeamCard = ({ data, deleteUserFromTeam, ChangeTeamName, AddUsers, DeleteTe
                 }
             }
         }
-    }
+    };
 
     const checkPermissionToDeleteRoles = (role) => {
         console.log(role);
@@ -87,112 +97,147 @@ const TeamCard = ({ data, deleteUserFromTeam, ChangeTeamName, AddUsers, DeleteTe
             }
         }
         return true;
-    }
+    };
 
-    return (
-        data.team_members.length === 0 ? (
-            <Loader />
-        ) : (
-            <div className='teamcard'>
-                <div className='teamcard-header'>
-                    <h2>{data.name}</h2>
-                    <div className='teamcard-subheader'>
-                        <p>Created by: {createdBy}</p>
-                        <p>Created at: {data.created_at}</p>
-                    </div>
+    return data.team_members.length === 0 ? (
+        <Loader />
+    ) : (
+        <div className='teamcard'>
+            <div className='teamcard-header'>
+                <h2>{data.name}</h2>
+                <div className='teamcard-subheader'>
+                    <p>Created by: {createdBy}</p>
+                    <p>Created at: {data.created_at}</p>
                 </div>
-                <div className='teamcard-body'>
-                    <div className='team-members'>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Username</th>
-                                    <th>Role</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.team_members.map((member) => (
-                                    <tr key={member.user.user_id}>
-                                        <td>
-                                            <h3>{member.user.username}</h3>
-                                        </td>
-                                        <td>
-                                            {member.roles.map((role) => (
-                                                <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }} key={role.team_members_role_id}>
-                                                    <p>
-                                                        {role.board_id !== null ? role.name + " in " + role.board.name : role.name}
-                                                    </p>
-                                                    {checkPermissonToManageTeam(data.team_id) && (
-                                                        <div>
-                                                            {checkPermissionToDeleteRoles(role) && (
-                                                                <button
-                                                                    className='delete_button'
-                                                                    onClick={() => handleDeleteRole(role.team_members_role_id, role.board_id, data.team_id, member.user.user_id)}
-                                                                >
-                                                                    Delete role
-                                                                </button>
-                                                            )}
-                                                        </div>
-                                                    )}
-                                                </div>
-                                            ))}
-                                        </td>
-                                        <td>
-                                            {checkPermissonToManageTeamMembers(data.team_id) && (
-                                                <div className='teamcard-actions'>
-                                                    {parseInt(member.user.user_id) !== user_id && (
-                                                        <button
-                                                            className='delete_button'
-                                                            onClick={() => deleteUserFromTeam(data.team_id, member.user.user_id)}
-                                                        >
-                                                            Remove user from team
-                                                        </button>
-                                                    )}
-                                                    {checkPermissonToManageTeam(data.team_id) && (
-                                                        <button className='manageButton' onClick={() => handleRolesButton(member.team_members_id)}>
-                                                            Add role
-                                                        </button>
-                                                    )}
-                                                </div>
-                                            )}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div>
-                        {checkPermissonToManageTeam(data.team_id) && (
-                            <div>
-                                <button className='manageButton' onClick={ManageTeam}>
-                                    Change team name
-                                </button>
-                                <button className='delete_button' onClick={handleDeleteButton}>
-                                    Delete Team
-                                </button>
-                            </div>
-                        )}
-                        {checkPermissonToManageTeamMembers(data.team_id) && (
-                            <button className='manageButton' onClick={handleAddButton}>
-                                Add Users
-                            </button>
-                        )}
-                    </div>
-                </div>
-                {manageIsClicked && (
-                    <TeamManager teamData={teamData} onClose={ManageTeam} ChangeTeamName={ChangeTeamName} />
-                )}
-                {deleteIsClicked && (
-                    <DeleteConfirm teamID={data.team_id} OnClose={handleDeleteButton} DeleteTeam={DeleteTeam} />
-                )}
-                {addIsClicked && <AddUser teamID={data.team_id} OnClose={handleAddButton} AddUsers={AddUsers} />}
-                {rolesIsClicked && (
-                    <RolesManager OnClose={handleRolesButton} team_id={data.team_id} team_member_id={teamMemberId} AddRoleToUser={AddRoleToUser} />
-                )}
             </div>
-        )
-    )
-}
+            <div className='teamcard-body'>
+                <div className='team-members'>
+                    {data.team_members.map((member) => (
+                        <div className='team-member-card'>
+                            <div className='team-member-image-container'>
+                                <img
+                                    className='team-member-image'
+                                    src={member.user.user_image} //ha null, akkor default kép
+                                    alt='profile'
+                                />
+                            </div>
+                            <div className='team-member-card-body'>
+                                <p>{member.user.username}</p>
+                                <p>{member.user.email}</p>
+                            </div>
+                        </div>
+                    ))}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.team_members.map((member) => (
+                                <tr key={member.user.user_id}>
+                                    <td>
+                                        <h3>{member.user.username}</h3>
+                                    </td>
+                                    <td>
+                                        {member.roles.map((role) => (
+                                            <div
+                                                style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}
+                                                key={role.team_members_role_id}
+                                            >
+                                                <p>
+                                                    {role.board_id !== null
+                                                        ? role.name + ' in ' + role.board.name
+                                                        : role.name}
+                                                </p>
+                                                {checkPermissonToManageTeam(data.team_id) && (
+                                                    <div>
+                                                        {checkPermissionToDeleteRoles(role) && (
+                                                            <button
+                                                                className='delete_button'
+                                                                onClick={() =>
+                                                                    handleDeleteRole(
+                                                                        role.team_members_role_id,
+                                                                        role.board_id,
+                                                                        data.team_id,
+                                                                        member.user.user_id
+                                                                    )
+                                                                }
+                                                            >
+                                                                Delete role
+                                                            </button>
+                                                        )}
+                                                    </div>
+                                                )}
+                                            </div>
+                                        ))}
+                                    </td>
+                                    <td>
+                                        {checkPermissonToManageTeamMembers(data.team_id) && (
+                                            <div className='teamcard-actions'>
+                                                {parseInt(member.user.user_id) !== user_id && (
+                                                    <button
+                                                        className='delete_button'
+                                                        onClick={() =>
+                                                            deleteUserFromTeam(data.team_id, member.user.user_id)
+                                                        }
+                                                    >
+                                                        Remove user from team
+                                                    </button>
+                                                )}
+                                                {checkPermissonToManageTeam(data.team_id) && (
+                                                    <button
+                                                        className='manageButton'
+                                                        onClick={() => handleRolesButton(member.team_members_id)}
+                                                    >
+                                                        Add role
+                                                    </button>
+                                                )}
+                                            </div>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div>
+                    {checkPermissonToManageTeam(data.team_id) && (
+                        <div>
+                            <button className='manageButton' onClick={ManageTeam}>
+                                Change team name
+                            </button>
+                            <button className='delete_button' onClick={handleDeleteButton}>
+                                Delete Team
+                            </button>
+                        </div>
+                    )}
+                    {checkPermissonToManageTeamMembers(data.team_id) && (
+                        <button className='manageButton' onClick={handleAddButton}>
+                            Add Users
+                        </button>
+                    )}
+                </div>
+            </div>
+            {manageIsClicked && (
+                <TeamManager teamData={teamData} onClose={ManageTeam} ChangeTeamName={ChangeTeamName} />
+            )}
+            {deleteIsClicked && (
+                <DeleteConfirm teamID={data.team_id} OnClose={handleDeleteButton} DeleteTeam={DeleteTeam} />
+            )}
+            {addIsClicked && <AddUser teamID={data.team_id} OnClose={handleAddButton} AddUsers={AddUsers} />}
+            {rolesIsClicked && (
+                <RolesManager
+                    OnClose={handleRolesButton}
+                    team_id={data.team_id}
+                    team_member_id={teamMemberId}
+                    AddRoleToUser={AddRoleToUser}
+                />
+            )}
+        </div>
+    );
+};
 export default TeamCard;
