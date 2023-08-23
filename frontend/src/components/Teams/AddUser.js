19cde02cbbb3a7ff062c4b67e9dd4bbc413b8f51
kovacsgabor0730 <kovacs.gabor.0730@gmail.com>
2023-08-23 13:50:40 +0200

    Teams Roles fix

--- frontend/src/components/Teams/AddUser.js
+++ frontend/src/components/Teams/AddUser.js
@@ -1,70 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from '../../api/axios';
-import { useEffect, useState } from 'react';
-
 
 const AddUser = ({ teamID, OnClose, AddUsers }) => {
-
-  const [users, SetUsers] = useState([]);
-  const [usersToTeams, SetUsersToTeams] = useState([]);
-
+  const [users, setUsers] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
   useEffect(() => {
     getUsers();
   }, []);
 
-  function handleAddUsers(usersToTeams, teamID) {
-    AddUsers(usersToTeams, teamID)
-    OnClose();
-  }
-
   async function getUsers() {
     const token = sessionStorage.getItem('token');
     try {
-      const response = await axios.get(`team/${teamID}/management/no_members`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          }
-        });
-      SetUsers(response.data.users);
-    }
-    catch (error) {
+      const response = await axios.get(`team/${teamID}/management/no_members`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUsers(response.data.users);
+    } catch (error) {
       console.log(error.response);
     }
-
   }
 
-  function AddUser(user_id) {
-    SetUsersToTeams(prevUsers => [...prevUsers, user_id]);
-    console.log(user_id);
+  function toggleUserSelection(user_id) {
+    if (selectedUsers.includes(user_id)) {
+      setSelectedUsers(selectedUsers.filter(id => id !== user_id));
+    } else {
+      setSelectedUsers([...selectedUsers, user_id]);
+    }
+  }
+
+  function handleAddUsers() {
+    AddUsers(selectedUsers, teamID);
+    OnClose();
   }
 
   return (
     <div className="overlay">
-      <div className='popup'>
+      <div className="popup">
         <div className="popup-content">
           <button className="close-btn" onClick={OnClose}>
             Close
           </button>
-          <p>Are you sure you want delete this team?</p>
+          <p>Select the users: </p>
           <table>
             <tbody>
               {users.map((user) => (
-                <tr>
-                  <td onClick={() => AddUser(user.user_id)}>
-                    {user.username}
-                  </td>
+                <tr key={user.user_id} onClick={() => toggleUserSelection(user.user_id)} className={selectedUsers.includes(user.user_id) ? 'selected' : ''}>
+                  <td>{user.username}</td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <button onClick={() => handleAddUsers(usersToTeams, teamID)}>Add Users</button>
+          <button onClick={handleAddUsers}>Add Users</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddUser;
-
