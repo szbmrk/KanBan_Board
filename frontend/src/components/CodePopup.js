aa378cdb6eed33fcfba7952950538cedf4585edf
s-mate <megamate95@gmail.com>
2023-08-24 10:47:13 +0200

    Board pass method wrongly bugfix Code popup set value wrongly bugfix CraftPromptPopup can handle reponse counter

--- frontend/src/components/CodePopup.js
+++ frontend/src/components/CodePopup.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Dropdown from 'react-dropdown';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import axios from '../api/axios';
-import '../styles/popup.css';
-import '../styles/CodePopup.css';
-import hljs from 'highlight.js';
+import React, { useState, useEffect } from "react";
+import Dropdown from "react-dropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import axios from "../api/axios";
+import "../styles/popup.css";
+import "../styles/CodePopup.css";
+import hljs from "highlight.js";
 
 const CodePopup = ({ board_id, onCancel }) => {
   const codeOptions = [
@@ -21,10 +21,10 @@ const CodePopup = ({ board_id, onCancel }) => {
   ];
   let [chosenAI, setChosenAI] = useState(aiOptions[0].value);
 
-    const [inputCode, setInputCode] = useState('');
-    const [output, setOutput] = useState('');
+  const [inputCode, setInputCode] = useState("");
+  const [output, setOutput] = useState("");
 
-    const closeIcon = <FontAwesomeIcon icon={faXmark} />;
+  const closeIcon = <FontAwesomeIcon icon={faXmark} />;
 
   const handleRunClick = async () => {
     try {
@@ -55,60 +55,59 @@ const CodePopup = ({ board_id, onCancel }) => {
     }
   };
 
-    useEffect(() => {
-        hljs.highlightAll();
-    });
+  useEffect(() => {
+    hljs.highlightAll();
+  });
 
-
-    return (
-        <div className='overlay'>
-            <div className='popup agi-popup'>
-                <span className='close-btn' onClick={onCancel}>
-                    {closeIcon}
-                </span>
-                <div className='gt-popup-content'>
-                    <h2>Coding popup</h2>
-                    <div className='coding-container'>
-                        <p>Your code:</p>
-                        <pre>
-                            <code
-                                className='code-textarea'
-                                contentEditable='true'
-                                spellCheck='true'
-                                value={inputCode}
-                                onChange={(e) => setInputCode(e.target.value)}
-                            />
-                        </pre>
-                    </div>
-                    <div className='gt-action-buttons'>
-                        <div className='dropdown-container'>
-                            <p>Which AI do you want to use?</p>
-                            <Dropdown
-                                className='code-dropdown'
-                                options={aiOptions}
-                                value={chosenAI}
-                                onChange={(selectedOption) => setChosenAI(selectedOption)}
-                            />
-                        </div>
-                        <div className='dropdown-container'>
-                            <p>Choose what you would like to do:</p>
-                            <Dropdown
-                                className='code-dropdown'
-                                options={codeOptions}
-                                value={selectedOption}
-                                onChange={(option) => setSelectedOption(option.value)}
-                                placeholder='Select an option'
-                            />
-                        </div>
-                        <button className='generate-button' onClick={handleRunClick}>
-                            Run
-                        </button>
-                    </div>
-                    <textarea className='output-textarea' value={output} disabled />
-                </div>
+  return (
+    <div className="overlay">
+      <div className="popup agi-popup">
+        <span className="close-btn" onClick={onCancel}>
+          {closeIcon}
+        </span>
+        <div className="gt-popup-content">
+          <h2>Coding popup</h2>
+          <div className="coding-container">
+            <p>Your code:</p>
+            <pre>
+              <code
+                className="code-textarea"
+                contentEditable="true"
+                spellCheck="true"
+                value={inputCode}
+                onChange={(e) => setInputCode(e.target.value)}
+              />
+            </pre>
+          </div>
+          <div className="gt-action-buttons">
+            <div className="dropdown-container">
+              <p>Which AI do you want to use?</p>
+              <Dropdown
+                className="code-dropdown"
+                options={aiOptions}
+                value={chosenAI}
+                onChange={(selectedOption) => setChosenAI(selectedOption.value)}
+              />
             </div>
+            <div className="dropdown-container">
+              <p>Choose what you would like to do:</p>
+              <Dropdown
+                className="code-dropdown"
+                options={codeOptions}
+                value={selectedOption}
+                onChange={(option) => setSelectedOption(option.value)}
+                placeholder="Select an option"
+              />
+            </div>
+            <button className="generate-button" onClick={handleRunClick}>
+              Run
+            </button>
+          </div>
+          <textarea className="output-textarea" value={output} disabled />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CodePopup;
