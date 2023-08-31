bb176c2b33d204cc86744a112f8a6bc8acf6ebb8
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2023-08-31 21:49:07 +0200

    craft prompt popup autocomplete display update

--- frontend/src/components/BasicAutocomplete.js
+++ frontend/src/components/BasicAutocomplete.js
@@ -1,51 +1,47 @@
-import React, { useState } from "react";
-import "../styles/BasicAutocomplete.css";
+import React, { useState } from 'react';
+import '../styles/BasicAutocomplete.css';
 
-const BasicAutocomplete = ({
-  placeholder,
-  selectedValue,
-  setSelectedValue,
-  behaviourOptions,
-  setBehaviourOptions,
-}) => {
-  const [value, setValue] = useState(selectedValue);
-  const [suggestions, setSuggestions] = useState([]);
+const BasicAutocomplete = ({ placeholder, selectedValue, setSelectedValue, behaviourOptions, setBehaviourOptions }) => {
+    const [value, setValue] = useState(selectedValue);
+    const [suggestions, setSuggestions] = useState([]);
+    const [visibility, setVisibility] = useState(['hidden']);
 
-  const handleInputChange = (e) => {
-    const inputValue = e.target.value;
-    setValue(inputValue);
-    setSelectedValue(inputValue);
+    const handleInputChange = (e) => {
+        const inputValue = e.target.value;
+        setValue(inputValue);
+        setSelectedValue(inputValue);
 
-    // Filter the items based on input value
-    const filteredSuggestions = behaviourOptions.filter((item) =>
-      item.toLowerCase().includes(inputValue.toLowerCase())
+        // Filter the items based on input value
+        const filteredSuggestions = behaviourOptions.filter((item) =>
+            item.toLowerCase().includes(inputValue.toLowerCase())
+        );
+        setSuggestions(filteredSuggestions);
+        setVisibility(['visible']);
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        setValue(suggestion);
+        setSelectedValue(suggestion);
+        setSuggestions([]);
+    };
+
+    return (
+        <div>
+            <input type='text' value={value} onChange={handleInputChange} placeholder={placeholder} />
+            <ul
+                className='suggestions-list'
+                style={{
+                    visibility: visibility,
+                }}
+            >
+                {suggestions.map((suggestion, index) => (
+                    <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                        {suggestion}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-    setSuggestions(filteredSuggestions);
-  };
-
-  const handleSuggestionClick = (suggestion) => {
-    setValue(suggestion);
-    setSelectedValue(suggestion);
-    setSuggestions([]);
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={handleInputChange}
-        placeholder={placeholder}
-      />
-      <ul className="suggestions-list">
-        {suggestions.map((suggestion, index) => (
-          <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
-            {suggestion}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 };
 
 export default BasicAutocomplete;
