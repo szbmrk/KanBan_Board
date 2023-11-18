b152be50ec2bb7784993a74721ff01a70a15e8a4
jani <bartusjani004@gmail.com>
2023-11-18 18:40:31 +0100

    Roles.js hiba javítás

--- frontend/src/roles/Roles.js
+++ frontend/src/roles/Roles.js
@@ -16,22 +16,6 @@ export const SetRoles = async (token) => {
     } catch (error) {
         console.log(error);
     }
-    const [theme, setTheme] = useState(sessionStorage.getItem("darkMode"));
-    useEffect(() => {
-        //ez
-        const ResetTheme = () => {
-            setTheme(sessionStorage.getItem("darkMode"))
-        }
-
-
-        console.log("Darkmode: " + sessionStorage.getItem("darkMode"))
-        window.addEventListener('ChangingTheme', ResetTheme)
-
-        return () => {
-            window.removeEventListener('ChangingTheme', ResetTheme)
-        }
-        //eddig
-    }, []);
 }
 
 export const checkPermisson = (team_id, permissionToCheck) => {
