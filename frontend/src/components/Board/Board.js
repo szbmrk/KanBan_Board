a21afc55990fb2b5b4c835774639dda479c9a0eb
szbmrk <szmark16@gmail.com>
2024-02-02 09:23:26 +0100

    task limit fix

--- frontend/src/components/Board/Board.js
+++ frontend/src/components/Board/Board.js
@@ -1944,8 +1944,6 @@ const Board = () => {
                                                         }}
                                                     >
                                                         <h2 className="card-title">{column.name}</h2>
-                                                        {(column.task_limit != 0 && column.task_limit != null) &&
-                                                            <p style={{ marginTop: "-25px", color: "var(--light-gray)" }}>{column.tasks.length + "/" + column.task_limit}</p>}
                                                     </div>
 
                                                     <div
@@ -1967,6 +1965,8 @@ const Board = () => {
                                                     </div>
                                                 </>
                                             )}
+                                            {(column.task_limit != 0 && column.task_limit != null) &&
+                                                <p style={{ marginTop: "-25px", color: "var(--light-gray)" }}>{column.tasks.length + "/" + column.task_limit}</p>}
                                             <div className="task-container">
                                                 {/*get tasks that match the filters*/}
                                                 {column.tasks.filter(task => {
