d33885a7c0175e31ef754859d56744fa00feb51c
szbmrk <szmark16@gmail.com>
2024-03-28 09:11:03 +0000

    a

---
+++ frontend/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Transpile modern JavaScript to support older browsers
+    '@babel/preset-react' // Transpile JSX syntax
+  ],
+  plugins: [
+    // Add any additional Babel plugins here
+  ]
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: false,
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
