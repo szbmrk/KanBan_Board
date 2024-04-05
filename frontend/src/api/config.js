0de8f4dad0c180b9de428ad12a30ba1bb086f7ba
szbmrk <szmark16@gmail.com>
2024-04-05 08:07:15 +0200

    configs

---
+++ frontend/src/api/config.js
@@ -0,0 +1,4 @@
+export const REACT_APP_PUSHER_APP_ID = "1775562";
+export const REACT_APP_PUSHER_KEY = "8eb4e31dcda66e538aef";
+export const REACT_APP_PUSHER_SECRET = "ac4ce4471d2bf20ec4c8";
+export const REACT_APP_PUSHER_CLUSTER = "eu";
