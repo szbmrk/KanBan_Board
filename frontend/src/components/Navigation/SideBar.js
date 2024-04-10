d7190e7ac7dabb7f46d26895de4c07bbaae8a91d
patrikcsak <95058980+patrikcsak@users.noreply.github.com>
2024-04-10 15:06:32 +0200

    A sidebar-on feltüntetni éppen melyik menüpontban vagyok
    Co-Authored-By: MacCroat <145100510+MacCroat@users.noreply.github.com>

--- frontend/src/components/Navigation/SideBar.js
+++ frontend/src/components/Navigation/SideBar.js
@@ -1,6 +1,6 @@
 // Sidebar component that using among the application
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 import '../../styles/sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTable, faSignOutAlt, faListCheck, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
@@ -12,6 +12,8 @@ const AssignedTasksIcon = <FontAwesomeIcon icon={faListCheck} />;
 const PeopleGroup = <FontAwesomeIcon icon={faPeopleGroup} />;
 const permissions = JSON.parse(sessionStorage.getItem('permissions'));
 const Sidebar = () => {
+    const location = useLocation();
+    const isBoardActive = location.pathname.includes('/board');
     const [theme, setTheme] = useState(localStorage.getItem("darkMode"));
     const { isLoggedIn, onLogout } = React.useContext(AuthContext);
     useEffect(() => {
@@ -35,28 +37,28 @@ const Sidebar = () => {
         <div className='sidebar col-2 sidebar-visible' data-theme={theme}>
             <div className='sidebar-menu'>
                 <ul>
-                    <li>
+                    <li className={location.pathname === '/dashboard' || isBoardActive ? 'active' : ''}>
                         <Link to='/dashboard'>
                             {homeIcon}
                             <span>Dashboard</span>
                         </Link>
                     </li>
 
-                    <li>
+                    <li className={location.pathname === '/assigned_tasks' ? 'active' : ''}>
                         <Link to='/assigned_tasks'>
                             {AssignedTasksIcon}
                             <span>Assigned Tasks</span>
                         </Link>
                     </li>
 
-                    <li>
+                    <li className={location.pathname === '/teams' ? 'active' : ''}>
                         <Link to='/teams'>
                             {PeopleGroup}
                             <span>Teams</span>
                         </Link>
                     </li>
 
-                    <li>
+                    <li className={location.pathname === '/permissiontable' ? 'active' : ''}>
                         <Link to='/permissiontable'>
                             {tableIcon}
                             <span>Permission table</span>
