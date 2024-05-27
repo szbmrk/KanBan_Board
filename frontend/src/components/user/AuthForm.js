be03458319bbdfedf2abc942549a7ee6c3b9059f
PetraPongo <pongopetra99@gmail.com>
2024-05-27 16:03:01 +0200

    Email ellenőrzés css javítása

--- frontend/src/components/user/AuthForm.js
+++ frontend/src/components/user/AuthForm.js
@@ -160,8 +160,8 @@ const AuthForm = ({
                                 placeholder="Enter your email"
                                 required
                             />
-                            {formData.email.trim() !== "" && emailError && (
-                                <div className="auth-error" style={{ 
+                           {formData.email.trim() !== "" && emailError && (
+                                <div className={`auth-error ${emailError === 'Email valid' ? 'auth-valid' : ''}`} style={{ 
                                     textShadow: emailError && isVibrating ? '0 0 10px red' : 'none',
                                     animation: emailError && isVibrating ? 'shake 0.5s linear infinite' : 'none'
                                 }}>
