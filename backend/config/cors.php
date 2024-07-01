e295edfe54a6779eaf50170d99878138982f2e5b
szbmrk <szmark16@gmail.com>
2024-07-01 13:10:25 +0200

    deleted duplicate lines

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,7 +7,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3000/*'],
 
     'allowed_origins_patterns' => [],
 
