50b4c69ac517482554ca78b551dbac2dae40c52b
Szabó Márk <szmark16@gmail.com>
2024-10-04 16:08:19 +0200

    Update TagController.php

--- backend/app/Http/Controllers/TagController.php
+++ backend/app/Http/Controllers/TagController.php
@@ -33,6 +33,11 @@ class TagController extends Controller
             return response()->json(['error' => 'Board not found.'], 404);
         }
 
+        if ($user->hasPermission('system_admin')) {
+            $tags = Tag::where('board_id', $boardId)->get();
+            return response()->json(['tags' => $tags], 200);
+        }
+
         if (!$board->team->teamMembers->contains('user_id', $user->user_id)) {
             return response()->json(['error' => 'You are not a member of the team that owns this board.'], 403);
         }
@@ -42,7 +47,7 @@ class TagController extends Controller
 
         return response()->json(['tags' => $tags], 200);
     }
-    
+
     public function store(Request $request, $boardId)
     {
         $user = auth()->user();
@@ -51,10 +56,10 @@ class TagController extends Controller
         }
 
         $board = Board::where('board_id', $boardId)
-                      ->whereHas('team.teamMembers', function ($query) use ($user) {
-                          $query->where('user_id', $user->user_id);
-                      })
-                      ->first();
+            ->whereHas('team.teamMembers', function ($query) use ($user) {
+                $query->where('user_id', $user->user_id);
+            })
+            ->first();
 
         if (!$board) {
             return response()->json(['error' => 'Board not found.'], 404);
@@ -84,12 +89,12 @@ class TagController extends Controller
 
         return response()->json(['message' => 'Tag created successfully.'], 201);
     }
-    
-    
+
+
     public function update(Request $request, $boardId, $tagId)
     {
         $user = auth()->user();
-        
+
         if (!$user) {
             return response()->json(['error' => 'Unauthorized.'], 404);
         }
@@ -107,8 +112,8 @@ class TagController extends Controller
         }
 
         $tag = Tag::where('tag_id', $tagId)
-                  ->where('board_id', $boardId)
-                  ->first();
+            ->where('board_id', $boardId)
+            ->first();
 
         if (!$tag) {
             return response()->json(['error' => 'Tag not found for the specified board.'], 404);
@@ -126,12 +131,12 @@ class TagController extends Controller
         if ($board->tags()->where('name', $request->input('name'))->where('tag_id', '!=', $tagId)->exists()) {
             return response()->json(['error' => 'A tag with the same name already exists for this board.'], 422);
         }
-    
-        
+
+
         if ($board->tags()->where('color', $request->input('color'))->where('tag_id', '!=', $tagId)->exists()) {
             return response()->json(['error' => 'A tag with the same color already exists for this board.'], 422);
         }
-        
+
         $tag->name = $request->input('name');
         $tag->color = $request->input('color');
         $tag->save();
@@ -141,7 +146,7 @@ class TagController extends Controller
         ];
         broadcast(new BoardChange($boardId, "UPDATED_TAG", $data));
 
-        $user_ids = UserTask::whereIn('task_id', function($query) use ($tagId) {
+        $user_ids = UserTask::whereIn('task_id', function ($query) use ($tagId) {
             $query->select('task_id')
                 ->from('task_tags')
                 ->where('tag_id', $tagId);
@@ -155,20 +160,21 @@ class TagController extends Controller
     }
 
 
-    public function destroy($boardId, $tagId) {
+    public function destroy($boardId, $tagId)
+    {
 
         $user = auth()->user();
 
         if (!$user) {
             return response()->json(['error' => 'Unauthorized.'], 404);
         }
-      
+
         $board = Board::where('board_id', $boardId)->first();
 
         if (!$board) {
             return response()->json(['error' => 'Board not found.'], 404);
         }
-        
+
         $team = $board->team;
 
         if (!$team->teamMembers->contains('user_id', $user->user_id)) {
@@ -176,14 +182,14 @@ class TagController extends Controller
         }
 
         $tag = Tag::where('tag_id', $tagId)
-                    ->where('board_id', $boardId)
-                    ->first();
-      
+            ->where('board_id', $boardId)
+            ->first();
+
         if (!$tag) {
-          return response()->json(['error' => 'Tag not found for the specified board.'], 404); 
+            return response()->json(['error' => 'Tag not found for the specified board.'], 404);
         }
 
-        $user_ids = UserTask::whereIn('task_id', function($query) use ($tagId) {
+        $user_ids = UserTask::whereIn('task_id', function ($query) use ($tagId) {
             $query->select('task_id')
                 ->from('task_tags')
                 ->where('tag_id', $tagId);
@@ -199,10 +205,10 @@ class TagController extends Controller
         foreach ($user_ids as $user_id) {
             broadcast(new AssignedTaskChange($user_id, "DELETED_TAG", $data));
         }
-      
+
         return response()->json(['message' => 'Tag deleted successfully.'], 200);
-      
-      }
-      
-      
+
+    }
+
+
 }
