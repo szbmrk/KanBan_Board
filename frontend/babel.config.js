14fcc316b2a51ce3e158431a57d6a3639a60e56c
szbmrk <szmark16@gmail.com>
2024-10-18 11:27:27 +0200

    react prod build settings

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env', // Transpile modern JavaScript to support older browsers
-    '@babel/preset-react' // Transpile JSX syntax
-  ],
-  plugins: [
-    // Add any additional Babel plugins here
-  ]
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        'react-app'
+    ],
+    plugins: [
+        'transform-remove-console',
+    ]
 };
