d9d3b8a338faab812bb1a20c4d297c6c6367a42a
szbmrk <szmark16@gmail.com>
2024-10-22 20:04:52 +0200

    Update AGIController.php

--- backend/app/Http/Controllers/AGIController.php
+++ backend/app/Http/Controllers/AGIController.php
@@ -389,7 +389,7 @@ class AGIController extends Controller
     {
         $user = auth()->user();
 
-        if (!isset($response) || !$response instanceof \Psr\Http\Message\ResponseInterface || $response->getStatusCode() != 200) {
+        if ($response instanceof \Psr\Http\Message\ResponseInterface && $response->getStatusCode() != 200) {
             return;
         }
 
