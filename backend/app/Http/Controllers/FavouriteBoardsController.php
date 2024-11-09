b22bab5032af4df7c5e711eb10848b34004691cc
matee8 <molnarmatenorbert@gmail.com>
2024-11-09 10:05:22 +0100

    GET /favourite/boards now returns board_name

--- backend/app/Http/Controllers/FavouriteBoardsController.php
+++ backend/app/Http/Controllers/FavouriteBoardsController.php
@@ -18,7 +18,17 @@ class FavouriteBoardsController extends Controller
             return response()->json(['error' => 'User not found'], 404);
         }
 
-        $favourites = $user->favouriteBoards()->get();
+        $favourites = $user->favouriteBoards()
+            ->with('board')
+            ->get()
+            ->map(function($favourite) {
+                  return [
+                    'id' => $favourite->id,
+                    'user_id' => $favourite->user_id,
+                    'board_id' => $favourite->board->board_id,
+                    'board_name' => $favourite->board->name,
+                  ];
+            });
 
         if ($favourites->isEmpty())
         {
